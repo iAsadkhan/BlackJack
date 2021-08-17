7a2de5ddf32dab4598fe007a3ac43c817a374056
iAsadkhan <iasadkhan0@gmail.com>
2021-08-18 02:58:05 +0500

    initial

---
+++ index.js
@@ -0,0 +1,69 @@
+let sum= 0
+let hasBlackJack = false
+let isAlive =true
+let message=""
+let messagenew=document.getElementById("message-el");
+let sumnew=document.getElementById("sum");
+let cardsnew= document.getElementById("cards");
+
+
+function startGame()
+{
+    isAlive = true
+    let firstCard = randomNum()
+    let secondCard = randomNum()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame()
+{
+    sumnew.textContent = "Sum: " + sum
+    cardsnew.textContent=`Cards: ${cards.map(item=>item)}`
+    if (sum <= 20) {
+        message="Do you want to draw a new card? 🙂" 
+    } else if (sum === 21) {
+        message="Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message="You're out of the game! 😭"
+        isAlive=false
+    }
+    messagenew.textContent = message
+}
+
+function randomNum()
+{
+    let num=Math.floor(Math.random()*13)+1
+    console.log(num)
+    if(num>10)
+    {
+        return 10
+    }
+    else if(num===1)
+    {
+        return 11
+    }
+    else
+    return num
+}
+
+function newCard()
+{
+    if(isAlive === true && hasBlackJack === false)
+    {
+        let card=randomNum()
+        console.log(card)
+        sum=sum+card
+        cards.push(card)
+        if(sum>21)
+        {
+            document.getElementById("button").disabled=true;
+        document.getElementById("gameover").textContent="GameOver"
+        }
+    renderGame();
+   
+    }
+
+}
